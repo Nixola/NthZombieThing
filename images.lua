672d4f66f254388d5cbd194956e266434bfdaa25
Nixola <niki.or97@yahoo.it>
2015-05-01 12:53:22 +0200

    Now underscores in images' name become slashes in images' path.

--- images.lua
+++ images.lua
@@ -1,6 +1,7 @@
 img = {}
 
 setmetatable(img, {__index = function(self, name)
+  name = name:gsub("_", "/")
   local r, img = pcall(love.graphics.newImage, "img/"..name..".png")
   if r then
     self[name] = img
