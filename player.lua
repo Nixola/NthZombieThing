b55769c35666bbbfafbcd0019734b8ca56a5b078
Nixola <niki.or97@yahoo.it>
2015-10-21 22:41:01 +0200

    Fixed mousewheel weapon selection

--- player.lua
+++ player.lua
@@ -191,14 +191,14 @@ player.keypressed = function(self, k)
 end
 
 player.mousepressed = function(self, x, y, b)
-  if b == "wu" then
+  if b == "wd" then
     local id = player.weapons.current.id
     id = id + 1
     if id > #player.weapons.list then
       id = id - #player.weapons.list
     end
     player.weapons:set(id)
-  elseif b == "wd" then
+  elseif b == "wu" then
     local id = player.weapons.current.id
     id = id - 1
     if id == 0 then
